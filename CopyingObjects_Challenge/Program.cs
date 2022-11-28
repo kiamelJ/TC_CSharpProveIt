bb44ef93ab2f3ac96bb8b1891c226bfdc61d33fa
Mike Jones <kiamel76@gmail.com>
2022-11-28 19:44:33 +0100

    added copyingObjects_challenge

---
+++ CopyingObjects_Challenge/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CopyingObjects_Challenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
