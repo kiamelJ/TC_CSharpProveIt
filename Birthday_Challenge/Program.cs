87a88d38e25bb84ac417e7a48ec21fc3c15aacb8
Mike Jones <kiamel76@gmail.com>
2023-02-05 19:15:50 +0100

    sunday practice

---
+++ Birthday_Challenge/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Birthday_Challenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Skulle nog behöva avsätta mer tid här men känner inte att det här är prio
+            // just nu så jag hoppar vidare bland utmaningarna. Återkom om tid finnes.
+            // Kort så kan man säga att det är inte exakt så lätt som man tror att räkna ut någons ålder.
+
+            //Console.WriteLine(Age());
+            //Console.WriteLine(AgeCalcOne());
+            //Console.WriteLine(AgeCalcTwo());
+            Console.WriteLine(AgeCalcChatGPT());
+
+
+            Console.ReadLine();
+        }
+
+        static private DateTime YearOfBirth()
+        {
+            var output = new DateTime(1976, 12, 28);
+            return output;
+        }
+
+        static private DateTime CurrentDate()
+        {
+            var output = DateTime.Now;
+            return output;
+        }
+
+        static private double Age()
+        {
+            var currentDate = CurrentDate();
+            var yearOfBirth = YearOfBirth();
+            var output = currentDate.Subtract(yearOfBirth);
+            return Math.Round(output.TotalDays);
+        }
+
+        static private int AgeCalcOne()
+        {
+            // This is not correct because it says that I have
+            // already had my 47 birthday
+            var dob = new DateTime(1976, 12, 28);
+            var age = DateTime.Now.Year - dob.Year;
+            return age;
+        }
+
+        static private int AgeCalcTwo()
+        {
+            // This gives the right number of years
+            var dob = new DateTime(1976, 12, 28);
+            var age = (int)((DateTime.Now - dob).TotalDays / 365.242199);
+            return age;
+        }
+
+        static private int AgeCalcChatGPT()
+        {
+            var dob = new DateTime(1976, 12, 28);
+            var now = DateTime.Now;
+
+            int age = now.Year - dob.Year;
+            if (now < dob.AddYears(age))
+            {
+                age--;
+            }
+            return age;
+        }
+
+    }
+}
---
+++ Generics_Challenge/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace Generics_Challenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+
+            List<string> maleList = new List<string>();
+            maleList.Add("Tom");
+            maleList.Add("John");
+            maleList.Add("Lukas");
+            maleList.Add("Kalle");
+            maleList.Add("Nils");
+            maleList.Add("Petter");
+
+
+            List<string> femaleList = new List<string>
+            {
+                "Jennifer",
+                "Sue",
+                "Ellen",
+                "Mary"
+            };
+
+            var hej = MergeLists(maleList, femaleList);
+
+            foreach (var item in hej)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.ReadLine();
+        }
+
+        // TC solution. Fortsätt och gå igenom exakt vad den här koden gör så du 
+        // greppar det lite iaf.
+        private static List<T> MergeLists<T>(List<T> list1, List<T> list2)
+        {
+            List<T> output = new List<T>();
+            List<T> biggerList;
+            List<T> smallerList;
+
+            if (list1.Count > list2.Count)
+            {
+                biggerList = list1;
+                smallerList = list2;
+            }
+            else
+            {
+                biggerList=list2;
+                smallerList=list1;
+            }
+
+            int smallerCount = smallerList.Count;
+
+            for (int i = 0; i < biggerList.Count; i++)
+            {
+                output.Add(biggerList[i]);
+
+                if (i < smallerCount)
+                {
+                    output.Add(smallerList[i]);
+                }
+            }
+
+            return output;
+        }
+
+        // Mitt försök. Det blev knas där med inparametrarna för jag kunde inte
+        // använda count på dom och då visste jag inte hur jag skulle kunna gå vidare.
+
+        //private static List<T> GenericMethod<T, U>(T maleList, U femaleList)
+        //{
+        //    var maleListLength = maleList.ToString().Count();
+        //    var femaleListLength = femaleList.ToString().Count();
+        //    var intermixed = new List<T>();
+
+        //    int i = 0;
+        //    int j = 0;
+
+        //    while (i < maleListLength && j < femaleListLength)
+        //    {
+        //        intermixed.Add(maleList[i]);
+        //    }
+
+
+        //    return Outputter;
+        //}
+
+        
+      
+        
+    }
+}
